086fcbe21195b4dcbf602b6d384d1471da86c8c3
Alexander <Alexan2405@gmail.com>
2015-03-27 19:22:10 +0100

    ferdig

--- Innlevering2/src/test/Oppgave2.java
+++ Innlevering2/src/test/Oppgave2.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Oppgave2 {
 	public static void main(String[] args) throws FileNotFoundException{
+		ArrayList<Person> personer = setPerson();
+		System.out.println("Alle personene: ");
+		showPersons(personer);
+		ArrayList<Person> students = getStudent();
+		System.out.println("Alle studentene: ");
+		showPersons(students);
+}
+	public static ArrayList<Person> setPerson() throws FileNotFoundException{	
 		Scanner input = new Scanner(new File("personer.txt"));
 		ArrayList<Person> Personer = new ArrayList<Person>();
 		while(input.hasNext()){
 			String person = input.next();
 			if(person.startsWith("S")){
-				System.out.println(person);
 				String studentnummer = person.split(",")[1];
 				String personnummer = person.split(",")[2];
 				String fornavn = person.split(",")[3];
@@ -20,7 +27,6 @@ public class Oppgave2 {
 				Personer.add(elev);
 			}
 			else if(person.startsWith("E")){
-				System.out.println(person);
 				String ansattnummer = person.split(",")[1];
 				String personnummer = person.split(",")[2];
 				String fornavn = person.split(",")[3];
@@ -30,9 +36,30 @@ public class Oppgave2 {
 				Personer.add(ansatt);
 			}
 		}
-		for(Person p : Personer){
-			System.out.println(p);
+		input.close();
+		return Personer;
+	}		
+	public static void showPersons(ArrayList<Person>personer){
+		for(Person p : personer){
+				System.out.println(p);
+			}
+	}
+	public static ArrayList<Person> getStudent() throws FileNotFoundException{
+		Scanner input = new Scanner(new File("personer.txt"));
+		ArrayList<Person> student = new ArrayList<Person>();
+		while(input.hasNext()){
+			String person = input.next();
+			if(person.startsWith("S")){
+				String studentnummer = person.split(",")[1];
+				String personnummer = person.split(",")[2];
+				String fornavn = person.split(",")[3];
+				String etternavn = person.split(",")[4];
+				int studiepoeng = Integer.parseInt(person.split(",")[5]);
+				Student elev = new Student(studentnummer, personnummer, fornavn, etternavn, studiepoeng);
+				student.add(elev);
+			}
 		}
 		input.close();
-	}		
+		return student;
+	}
 }
